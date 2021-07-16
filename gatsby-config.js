992076ae115f9d856f9b707db57bfa0a47062930
Nick Lee <raven1300@hotmail.com>
2021-07-16 17:51:31 +1000

    rest of files

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,5 +6,29 @@
 
 module.exports = {
   /* Your site config here */
-  plugins: [],
+  plugins: [
+    "gatsby-transformer-remark",
+    "gatsby-transformer-sharp",
+    "gatsby-plugin-sharp",
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `projects`,
+        path: `${__dirname}/src/projects/`,
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images/`,
+      },
+    },
+  ],
+  siteMetadata: {
+    title: "Raven Dev", 
+    description: "Raven Dev: blog and portfolio", 
+    copyright: "This website is copyright 2021 Raven",
+    contact: "me@raven.click"
+  }
 }
---
+++ src/pages/about.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Layout from "../components/Layout"
+
+export default function About() {
+    return (
+        <Layout>
+            <div>
+                <h1>About Page</h1>  
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus vero incidunt earum aperiam beatae dignissimos velit, expedita hic eos explicabo qui, voluptas nihil ad error facere, officiis mollitia corrupti accusamus?</p>  
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus vero incidunt earum aperiam beatae dignissimos velit, expedita hic eos explicabo qui, voluptas nihil ad error facere, officiis mollitia corrupti accusamus?</p>  
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus vero incidunt earum aperiam beatae dignissimos velit, expedita hic eos explicabo qui, voluptas nihil ad error facere, officiis mollitia corrupti accusamus?</p>  
+            </div>
+        </Layout>
+    )
+}
